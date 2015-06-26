fa60bea67c8577e28acd5ba64e4365c0f064dee2
chronolai <chronolai@synology.com>
2015-06-26 14:46:06 +0800

    refactor

--- index.php
+++ index.php
@@ -2,68 +2,27 @@
 include "config.php";
 include "slackbot.php";
 
-$slackbot = new Slackbot($_REQUEST);
+$slackbot = new Slackbot(BOT_NAME, $_REQUEST);
 
 $slackbot->valid('token', TOKEN, false);
 $slackbot->valid('user_id', 'USLACKBOT');
 $slackbot->valid('user_name', 'slackbot');
 $slackbot->valid('text', '');
 
-$slackbot->setList('restaurants', [
-	"京讚",
-	"京站",
-	"珍珍水餃, 還可以買甜甜圈",
-	"八方",
-	"采岩堂",
-	"日本人咖哩",
-	"炒飯  (記得訂位 - 02 2521 6619 ^.<)",
-	"新東陽",
-	"新光三越",
-	"永井壽司",
-	"排骨酥",
-	"摩斯",
-	"王家漢堡"
+$slackbot->setList('test', [
+	"測尛? 我還沒死...",
+	"當機我就不會回你了阿!!!",
 ]);
 
-$slackbot->setList('drinks', [
-	"50嵐",
-	"康青龍",
-	"水巷茶弄",
-	"星巴克",
-	"冰箱的飲料",
-	"水"
-]);
-
-$slackbot->resp('test|測試', function($data) {
-	$data['username'] = 'chronolai-bot';
-	$data['text'] = "測試什麼拉? 我還沒死....";
+$slackbot->add('test|測試', function() use($slackbot) {
+	$data['username'] = 'robot';
+	$data['text'] = $slackbot->getRandListItem('test');
 	return $data;
 });
 
-$slackbot->resp('天氣|氣象', function($data) {
-	$data['username'] = '氣象主播 - 刃利魚';
-
-	$xml = simplexml_load_file("http://opendata.cwb.gov.tw/opendataapi?dataid=F-C0032-009&authorizationkey=CWB-E1DBB1C2-0A6A-4EB0-9BE6-358053080C85");
-	if (false === $xml) {
-		return;
-	}
-	$data['text'] = "";
-	$data['text'] .= $xml->dataset->location->locationName."\n";
-	foreach ($xml->dataset->parameterSet->parameter as $param) {
-		$data['text'] .= $param->parameterValue."\n";
-	}
-	return $data;
-});
-
-$slackbot->resp('吃啥|吃什麼|肚子餓|午餐', function($data) use ($slackbot) {
-	$data['username'] = '食戟のソーマ - 幸平 創真';
-	$data["text"] = "吃".$slackbot->getRandListItem('restaurants');
-	return $data;
-});
-
-$slackbot->resp('喝啥|喝什麼|飲料|口渴', function($data) use ($slackbot) {
-	$data['username'] = '飲料達人';
-	$data["text"] = "喝".$slackbot->getRandListItem('drinks');
+$slackbot->add('叫(?<times>\d+)聲', function($times) {
+	$data['username'] = 'robot-cat';
+	$data['text'] = str_repeat("Meow! ", $times);
 	return $data;
 });
 
--- slackbot.php
+++ slackbot.php
@@ -1,9 +1,10 @@
 <?php
 class Slackbot
 {
-	function __construct($req)
+	function __construct($name, $input)
 	{
-		$this->req = $req;
+		$this->name = $name;
+		$this->input = $input;
 		$this->list = [];
 		$this->data = [];
 	}
@@ -13,30 +14,9 @@ class Slackbot
 	}
 	public function valid($key, $value, $cond = true)
 	{
-		if (($this->req[$key] === $value) === $cond) {
+		if (($this->input[$key] === $value) === $cond) {
 			exit();
 		}
-	}
-	public function resp($keyword, $callback)
-	{
-		if (preg_match("/" . $keyword . "/", $this->req['text'], $args)) {
-			$this->data[] = call_user_func_array($callback, [$this->data]);
-		}
-	}
-	public function run()
-	{
-		$data = [];
-		if (0 == count($this->data)) {
-			$data['username'] = $this->data[0]['username'];
-			$data['text'] = $this->data[0]['text'];
-		} else {
-			for ($i=0; $i < count($this->data); $i++) {
-				$username = $this->data[$i]['username'];
-				$text = $this->data[$i]['text'];
-				@$data['text'] .= ($i>0?"\n----\n":'').$username.":\n".$text;
-			}
-		}
-		echo json_encode($data);
 	}
 	public function getList($name)
 	{
@@ -50,5 +30,33 @@ class Slackbot
 	{
 		$rand_key = array_rand($this->list[$name]);
 		return $this->list[$name][$rand_key];
+	}
+	public function add($keyword, $callback)
+	{
+		$this->rules["/" . $keyword . "/"] = $callback;
+		return $this;
+	}
+	public function run()
+	{
+		foreach ($this->rules as $rule => $callback) {
+			$args = [];
+			if (preg_match($rule, $this->input['text'], $args)) {
+				// remove int index
+				foreach ($args as $key => $value) {
+					if (is_numeric($key)) {
+						unset($args[$key]);
+					}
+				}
+				// end
+				$this->data[] = call_user_func_array($callback, $args);
+			}
+		}
+		$resp= ['username'=>$this->name];
+		for ($i=0; $i < count($this->data); $i++) {
+			$username = @$this->data[$i]['username'];
+			$text = @$this->data[$i]['text'];
+			@$resp['text'] .= ($i>0?"\n----\n":'').$username.":\n".$text;
+		}
+		echo json_encode($resp);
 	}
 }
