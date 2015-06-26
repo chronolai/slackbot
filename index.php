aff4ee9114c8f78543491eb67bab9d64ae253ee2
chronolai <chronolai@synology.com>
2015-06-26 09:41:29 +0800

    add getList, setList, getRandListItem & update config.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
+include "config.php";
 include "slackbot.php";
-
-define('TOKEN', 'your_token');
 
 $slackbot = new Slackbot($_REQUEST);
 
@@ -10,7 +9,38 @@ $slackbot->valid('user_id', 'USLACKBOT');
 $slackbot->valid('user_name', 'slackbot');
 $slackbot->valid('text', '');
 
-$slackbot->resp('天氣|氣象', function($data){
+$slackbot->setList('restaurants', [
+	"京讚",
+	"京站",
+	"珍珍水餃, 還可以買甜甜圈",
+	"八方",
+	"采岩堂",
+	"日本人咖哩",
+	"炒飯  (記得訂位 - 02 2521 6619 ^.<)",
+	"新東陽",
+	"新光三越",
+	"永井壽司",
+	"排骨酥",
+	"摩斯",
+	"王家漢堡"
+]);
+
+$slackbot->setList('drinks', [
+	"50嵐",
+	"康青龍",
+	"水巷茶弄",
+	"星巴克",
+	"冰箱的飲料",
+	"水"
+]);
+
+$slackbot->resp('test|測試', function($data) {
+	$data['username'] = 'chronolai-bot';
+	$data['text'] = "測試什麼拉? 我還沒死....";
+	return $data;
+});
+
+$slackbot->resp('天氣|氣象', function($data) {
 	$data['username'] = '氣象主播 - 刃利魚';
 
 	$xml = simplexml_load_file("http://opendata.cwb.gov.tw/opendataapi?dataid=F-C0032-009&authorizationkey=CWB-E1DBB1C2-0A6A-4EB0-9BE6-358053080C85");
@@ -25,38 +55,15 @@ $slackbot->resp('天氣|氣象', function($data){
 	return $data;
 });
 
-$slackbot->resp('吃啥|吃什麼|肚子餓|午餐', function($data){
+$slackbot->resp('吃啥|吃什麼|肚子餓|午餐', function($data) use ($slackbot) {
 	$data['username'] = '食戟のソーマ - 幸平 創真';
-	$restaurants = [
-		"京讚",
-		"京站",
-		"珍珍水餃, 還可以買甜甜圈",
-		"八方",
-		"采岩堂",
-		"日本人咖哩",
-		"炒飯  (記得訂位 - 02 2521 6619 ^.<)",
-		"新東陽",
-		"新光三越",
-		"永井壽司",
-		"排骨酥",
-		"摩斯",
-		"王家漢堡"
-	];
-	$data["text"] = "吃".$restaurants[rand()%count($restaurants)];
+	$data["text"] = "吃".$slackbot->getRandListItem('restaurants');
 	return $data;
 });
 
-$slackbot->resp('喝啥|喝什麼|飲料|口渴', function($data){
+$slackbot->resp('喝啥|喝什麼|飲料|口渴', function($data) use ($slackbot) {
 	$data['username'] = '飲料達人';
-	$drinks = [
-		"50嵐",
-		"康青龍",
-		"水巷茶弄",
-		"星巴克",
-		"冰箱的飲料",
-		"水"
-	];
-	$data["text"] = "喝".$drinks[rand()%count($drinks)];
+	$data["text"] = "喝".$slackbot->getRandListItem('drinks');
 	return $data;
 });
 
--- slackbot.php
+++ slackbot.php
@@ -4,6 +4,7 @@ class Slackbot
 	function __construct($req)
 	{
 		$this->req = $req;
+		$this->list = [];
 		$this->data = [];
 	}
 	public function debug($value='')
@@ -19,16 +20,35 @@ class Slackbot
 	public function resp($keyword, $callback)
 	{
 		if (preg_match("/" . $keyword . "/", $this->req['text'], $args)) {
-			$this->data[] = call_user_func_array($callback, [$data]);
+			$this->data[] = call_user_func_array($callback, [$this->data]);
 		}
 	}
 	public function run()
 	{
 		$data = [];
-		for ($i=0; $i < count($this->data); $i++) { 
-			$data['username'] .= ($i>0?', ':'').$this->data[$i]['username'];
-			$data['text'] .= ($i>0?"\n----\n":'').$this->data[$i]['text'];
+		if (0 == count($this->data)) {
+			$data['username'] = $this->data[0]['username'];
+			$data['text'] = $this->data[0]['text'];
+		} else {
+			for ($i=0; $i < count($this->data); $i++) {
+				$username = $this->data[$i]['username'];
+				$text = $this->data[$i]['text'];
+				@$data['text'] .= ($i>0?"\n----\n":'').$username.":\n".$text;
+			}
 		}
 		echo json_encode($data);
+	}
+	public function getList($name)
+	{
+		return $this->list[$name];
+	}
+	public function setList($name, $list)
+	{
+		$this->list[$name] = $list;
+	}
+	public function getRandListItem($name)
+	{
+		$rand_key = array_rand($this->list[$name]);
+		return $this->list[$name][$rand_key];
 	}
 }
